98913907731a620d3a0db262568db1e1d55d00b8
Feike Hof <125443358+Feike-Hof@users.noreply.github.com>
2023-08-18 21:31:25 +0200

    feat: new spider

--- my_scraper/spiders/boardgame_geek.py
+++ my_scraper/spiders/boardgame_geek.py
@@ -4,7 +4,7 @@ import scrapy
 class BoardgameGeekSpider(scrapy.Spider):
     name = "boardgame_geek"
     allowed_domains = ["boardgamegeek.com"]
-    start_urls = ["https://boardgamegeek.com"]
+    start_urls = ["https://boardgamegeek.com/boardgame/224517/brass-birmingham"]
 
     def parse(self, response: scrapy.http.response.Response, **kwargs):
         with open('reponse.html', 'w') as file:
